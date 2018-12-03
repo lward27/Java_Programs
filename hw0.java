cd018f9c3a4117f328a2de93dbbfa2489182a9e6
Lucas Ward <33394997+wardl1990@users.noreply.github.com>
2018-12-03 14:32:50 -0500

    Create hw0.java

---
+++ hw0.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class hw0 {
+  public static void main(String args[]) {
+    short a;			//declare 'a'
+    a = 20000;			//set to 20000
+    a = (short)(a + a);		//double 'a'
+    System.out.println(a + "\n");
+  }
+} 
