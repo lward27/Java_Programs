0a6d9204f85df63253a23abc88f69b7e3ab25de5
Lucas Ward <33394997+wardl1990@users.noreply.github.com>
2018-12-03 10:19:54 -0500

    Create Friend.java

---
+++ Friend.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package edu.ncsu.csc.csc216.acronym_annihilator;
+
+/**
+ * @author Robert Atkinson
+ * @author Pierce Ellis
+ * @author William Fisher
+ * @author Lucas Ward
+ * @author Lydia Peedin
+ * @version 1.0
+ *
+ */
+public class Friend {
+	
+	
+	public static String findAndReplace(String msg) {
+		return msg;
+	}
+
+}
