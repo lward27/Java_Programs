3c79ab0c4d3e274d479e9d8a390a672544a5bf1e
Lucas Ward <33394997+wardl1990@users.noreply.github.com>
2018-12-03 12:48:28 -0500

    Create TriangleLooper.java

---
+++ TriangleLooper.java
@@ -0,0 +1,14 @@
+public class TriangleLooper
+{
+	public static void main(String[] args)
+	{
+		for(int i = 1; i<=5; i++)
+		{
+			for(int j = 1; j<=i-1; j++)
+				System.out.print(" ");
+			for(int j = 1; j <= 11 - 2*i; j++)
+				System.out.print("*");
+			System.out.println();
+		}
+	}
+}
