1c169d1ec9edcbe0c440c8f652069e8c83d2f5c8
Lucas Ward <33394997+wardl1990@users.noreply.github.com>
2018-12-03 12:36:19 -0500

    Create RomanNumerals.java

---
+++ RomanNumerals.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class RomanNumerals
+{
+	public static void main(String[] args)
+	{
+		Scanner console = new Scanner(System.in);
+		System.out.print("enter a number to roman numeralize: ");
+		int i = console.nextInt();
+		romanNumeralize(i);
+	}
+
+	public static void romanNumeralize(int i)
+	{
+		if(i <= 10)
+		{
+		
+		}
+	}
+	
+}
