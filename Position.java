bdb99fa4de5467d455e6566514f7fea83bb5a180
Lucas Ward <33394997+wardl1990@users.noreply.github.com>
2018-12-03 12:47:53 -0500

    Create Position.java

---
+++ Position.java
@@ -0,0 +1,19 @@
+/**
+ * S_0 = 10
+ * V_0 = 25
+ * a = 5
+ * t = 7
+ * @author JUST ME!! (Lucas Ward)
+ */
+
+public class Position
+{
+	public static void main(String[] args)
+	{
+		double s_0 = 10;
+		double v_0 = 25;
+		double a = 5;
+		double t = 7;
+		System.out.println(s_0 + (v_0 * t) + (0.5)*(a*t*t));
+	}
+}
