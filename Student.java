3c24f1cc23613de17b4b948db87cf7391690d251
Lucas Ward <33394997+wardl1990@users.noreply.github.com>
2018-12-03 13:29:40 -0500

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student extends Person
+{
+     private int grade;
+     public Student(String _lastName, String _firstName, String _middleName)
+     {
+          super(_lastName, _firstName, _middleName);
+          grade = 11;
+     }
+}
