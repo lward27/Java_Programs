8d6cf77337c6c288a4eb052e9ee11e865929c9ce
Lucas Ward <33394997+wardl1990@users.noreply.github.com>
2018-12-03 10:10:28 -0500

    Create Fermentable.java

---
+++ Fermentable.java
@@ -0,0 +1,14 @@
+package edu.ncsu.csc.csc216.heckman_brewery.beer;
+
+/**
+ * Interface that fermentable beverages should implement
+ * @author SarahHeckman
+ */
+public interface Fermentable {
+	
+	/**
+	 * Behavior for a fermentable beverage
+	 */
+	public void ferment();
+
+}
