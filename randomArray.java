7bd9b82f1bd014df6c57525efba6f134a1417e2e
Lucas Ward <33394997+wardl1990@users.noreply.github.com>
2018-12-03 13:04:44 -0500

    Create randomArray.java

---
+++ randomArray.java
@@ -0,0 +1,9 @@
+/*static int[] randomArray(int range)
+{
+  int[] newArray = new int[range];
+  for(int i = 0; i < range; i++)
+  {
+    newArray[i] = (int)(Interger.MAX_VALUE * Math.random());
+  }
+  return newArray;
+}*/
