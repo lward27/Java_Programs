a6327c020d049383813aaddd44493dc94f98ae87
Lucas Ward <33394997+wardl1990@users.noreply.github.com>
2018-12-03 14:31:37 -0500

    Create TrumanDriver.java

---
+++ TrumanDriver.java
@@ -0,0 +1,11 @@
+public class TrumanDriver
+{
+     public static void main(String [] args)
+     {
+          Truman t = new Truman();
+          try{
+               t.doSquat();
+          }
+          catch(Exception ex){}
+     }
+}
