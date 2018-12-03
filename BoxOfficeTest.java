c671cb752e16a7620b58dd831e1f8aa8530f7790
Lucas Ward <33394997+wardl1990@users.noreply.github.com>
2018-12-03 12:43:53 -0500

    Create BoxOfficeTest.java

---
+++ BoxOfficeTest.java
@@ -0,0 +1,78 @@
+public class BoxOfficeTest
+{
+	public static void main(String[] args)
+	{
+		testBoxOffice1();		
+		testBoxOffice2();		
+		testBoxOffice3();		
+		testBoxOffice4();		
+		testBoxOffice5();		
+		testBoxOffice6();		
+		testBoxOffice7();		
+		testBoxOffice8();		
+		testBoxOffice9();		
+		testBoxOffice10();		
+	}
+	
+
+	public static void testBoxOffice1()
+	{
+		System.out.println("Expected: true ");
+		System.out.print("Actual: ");
+		BoxOffice.getCost("AVATAR",4,3,true,2,2);
+	}
+
+	public static void testBoxOffice2()
+	{
+		System.out.println("Expected: Cost: ");
+		System.out.print("Actual: ");
+	}
+
+	public static void testBoxOffice3()
+	{
+		System.out.println("Expected: Cost: ");
+		System.out.print("Actual: ");
+	}
+
+	public static void testBoxOffice4()
+	{
+		System.out.println("Expected: Cost: ");
+		System.out.print("Actual: ");
+	}
+
+	public static void testBoxOffice5()
+	{
+		System.out.println("Expected: Cost: ");
+		System.out.print("Actual: ");
+	}
+
+	public static void testBoxOffice6()
+	{
+		System.out.println("Expected: Cost: ");
+		System.out.print("Actual: ");
+	}
+
+	public static void testBoxOffice7()
+	{
+		System.out.println("Expected: Cost: ");
+		System.out.print("Actual: ");
+	}
+
+	public static void testBoxOffice8()
+	{
+		System.out.println("Expected: Cost: ");
+		System.out.print("Actual: ");
+	}
+
+	public static void testBoxOffice9()
+	{
+		System.out.println("Expected: Cost: ");
+		System.out.print("Actual: ");
+	}
+
+	public static void testBoxOffice10()
+	{
+		System.out.println("Expected: Cost: ");
+		System.out.print("Actual: ");
+	}
+}
