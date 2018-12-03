051b63e45da61037ceafd1fee067633aeb3bcae8
Lucas Ward <33394997+wardl1990@users.noreply.github.com>
2018-12-03 10:19:33 -0500

    Create Professional.java

---
+++ Professional.java
@@ -0,0 +1,29 @@
+package edu.ncsu.csc.csc216.acronym_annihilator;
+
+/**
+ * @author Robert Atkinson
+ * @author Pierce Ellis
+ * @author William Fisher
+ * @author Lucas Ward
+ * @author Lydia Peedin
+ *
+ */
+public class Professional {
+	
+	/**
+	 * Finds and replaces all available acronyms
+	 * @param msg Message to be searched and replaced
+	 * @return Message with appropriate acronyms expanded.
+	 */
+	public static String findAndReplace(String msg) {
+		msg = Lmao.findAndReplace(msg);
+		msg = WRUD.findAndReplace(msg);
+		msg = WTF.findAndReplace(msg);
+		msg = IDK.findAndReplace(msg);
+		msg = BFF.findAndReplace(msg);
+		
+		return msg;
+	}
+	
+
+}
