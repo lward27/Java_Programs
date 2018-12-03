fb7b9aaecb7dee4199d47e2404a30d7bd7b24df1
Lucas Ward <33394997+wardl1990@users.noreply.github.com>
2018-12-03 10:15:14 -0500

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,14 @@
+package edu.ncsu.csc.csc216.acronym_annihilator;
+
+/**
+ * Constants for the labels
+ * @author SarahHeckman
+ */
+public class Constants {
+	
+	public static final String FAMILY_LABEL = "Family";
+	public static final String FRIEND_LABEL = "Friend";
+	public static final String PROFESSIONAL_LABEL = "Professional";
+	public static final String OTHER_LABEL = "Other";
+
+}
