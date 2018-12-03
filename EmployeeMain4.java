4fb740499590a0421ff3ddc7ca795ae0207efbba
Lucas Ward <33394997+wardl1990@users.noreply.github.com>
2018-12-03 13:59:16 -0500

    Create EmployeeMain4.java

---
+++ EmployeeMain4.java
@@ -0,0 +1,20 @@
+// Client code number 4
+public class EmployeeMain4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Employee[] employees = {new Employee(), new Lawyer(),
+				new Marketer(), new Secretary()};
+	
+		//print information about each employee
+		for(int i = 0; i < employees.length; i++) {
+			employees[i].applyForVacation();
+			employees[i].showHours();
+			employees[i].showSalary();
+			employees[i].showVacation();
+			System.out.println();
+		}
+	}
+}
